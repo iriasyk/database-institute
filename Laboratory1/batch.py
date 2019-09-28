89b13bf85377b9754d98525f44103b47d58b8394
Ihor Riasyk <riasik123400@gmail.com>
2019-09-29 00:59:13 +0300

    first push labwork1 with database

---
+++ Laboratory1/batch.py
@@ -0,0 +1,16 @@
+from cassandra.cluster import Cluster
+
+cluster = Cluster()
+connection = cluster.connect('labwork1BD')
+
+batch = """
+BEGIN BATCH
+INSERT INTO student (email, full_name)
+VALUES ('user1@gmail.com', {surname:'Riasyk', name:'Ihor', patronymic:'Serhiyovych'});
+
+INSERT INTO data_form (email, name_data_form, answers_on_questions, adding_comment)
+VALUES ('user2@gmail.com', 'data form2', 'answer1: c)', 'everything suits me...');
+
+APPLY BATCH;
+"""
+connection.execute(batch)
